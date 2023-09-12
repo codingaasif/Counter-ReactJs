cf73c7d58c32e765475357cede6f448569cdbbad
Aasif Noor <aasifnoor98@gmail.com>
2023-09-12 19:40:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+
+  // let counter = 0
+
+  const addValue = () => {
+    console.log("value added", counter);
+    counter = counter + 1;
+    setCounter(counter)
+    setCounter(counter)
+    setCounter(counter)
+    setCounter(counter)
+  }
+
+  const removeValue = () => {
+    if (counter) {
+      setCounter(counter - 1);
+    }else{
+      setCounter(0);
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur vite-react</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br/><br/>
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
